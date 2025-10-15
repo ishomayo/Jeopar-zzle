418e339abb447318d7daad6ac81990ee984db361
ishomayo <esguerraeugene98@gmail.com>
2025-10-16 05:40:04 +0800

    .

--- src/GameApplication.java
+++ src/GameApplication.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.scene.Scene;
 
 public class GameApplication extends Application {
     
@@ -17,14 +17,13 @@ public class GameApplication extends Application {
         primaryStage.setHeight(600);
         primaryStage.setResizable(false);
         
+
         
-        // Start with splash screen
         SplashScreen splashScreen = new SplashScreen(primaryStage);
         Scene splashScene = new Scene(splashScreen.getRoot(), 1000, 600);
         primaryStage.setScene(splashScene);
         primaryStage.show();
         
-        // Transition to lobby after splash screen
         splashScreen.setOnSplashComplete(() -> {
             LobbyScreen lobbyScreen = new LobbyScreen(primaryStage);
             Scene lobbyScene = new Scene(lobbyScreen.getRoot(), 1000, 600);
